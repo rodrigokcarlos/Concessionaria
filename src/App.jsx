32bf656ddb461e6ccc633d0f0f6871974d158be4
rodrigo <rodrigo.kazan15@gmail.com>
2023-01-12 21:00:45 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Corpo from './Corpo';
+
+function App() {
+  return (
+    <div className="App">
+     < Header />
+     <Corpo/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
